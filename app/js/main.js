aea7ab2e76fcd8b9746e6156224f45c9e2a1d157
Vlad <belogorod5@gamil.com>
2022-10-03 16:50:58 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+//     $('.slider').slick({
+//         autoplay: true,
+//         autoplaySpeed: 4000,
+//         dotsClass: String,
+//         slidesToShow: 1,
+//         adaptiveHeight: true,
+//         centerMode: true,
+//         dots: true,
+//     });
+// });
