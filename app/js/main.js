dc04d91f1fe5a5eba432a1cfa81a3b44cd77a43e
Vlad <belogorod5@gamil.com>
2022-10-13 15:33:45 +0300

    commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,21 @@
-// $(document).ready(function(){
-//     $('.slider').slick({
-//         autoplay: true,
-//         autoplaySpeed: 4000,
-//         dotsClass: String,
-//         slidesToShow: 1,
-//         adaptiveHeight: true,
-//         centerMode: true,
-//         dots: true,
-//     });
-// });
+$(document).ready(function() {
+    $('.carousel__slides').slick({
+        speed: 1500,
+        arrows: true,
+        slidesToShow: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/solutions/prev-arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/solutions/next-arrow.png"></button>',
+    });
+});
+
+
+$(document).ready(function() {
+    $('.promo__slides').slick({
+        speed: 1000,
+        arrows: true,
+        slidesToShow: 1,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/promo/prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/promo/next.png"></button>',
+    });
+});
